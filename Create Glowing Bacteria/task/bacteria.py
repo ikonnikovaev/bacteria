ec06a6291753bc8bc382d4b68f8e4a32f354d271
elena <ikonnikovaev@gmail.com>
2022-05-21 12:19:15 +0300

    Stage 4: Cut GFP to make sticky ends

--- Create Glowing Bacteria/task/bacteria.py
+++ Create Glowing Bacteria/task/bacteria.py
@@ -17,10 +17,19 @@ def complementary_strand(strand):
     letters = list(strand)
     return ''.join([complementary_letter(l) for l in letters])
 
-RESTRICTION_SITE = 'CTGCAG'
-strand = input()
-print(strand)
-print(complementary_strand(strand))
+# RESTRICTION_SITE = 'CTGCAG'
+gfp_original = input().strip()
+gfp_complementary = complementary_strand(gfp_original)
+restriction_sites = input().split()
+
+l_original = gfp_original.find(restriction_sites[0])
+r_original = gfp_original.rfind(restriction_sites[1])
+print(gfp_original[l_original + 1 : r_original + 1])
+
+l_complementary = gfp_complementary.find(complementary_strand(restriction_sites[0]))
+r_complementary = gfp_complementary.find(complementary_strand(restriction_sites[1]))
+print(gfp_complementary[l_complementary + 5 : r_complementary + 5])
+
 '''
 first_strand, second_strand = input().split()
 i = first_strand.find(RESTRICTION_SITE)
